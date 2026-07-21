9859f8f9cec37ff09ebf2725d7f50e40bccb6356
Ritesh Srivastav <Riteshshriv03@gmail.com>
2026-07-21 12:41:27 +0530

    feat(posts): implement feed API with ordered post listing

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreatePostView
+from .views import CreatePostView , FeedView
 
 
 urlpatterns = [
     path("", CreatePostView.as_view(), name="create-post"),
+    path("feed/",FeedView.as_view(), name = "feed")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView , ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Post
@@ -14,3 +14,13 @@ class CreatePostView(CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    
+class FeedView(ListAPIView):
+    """API view for listing all posts."""
+    # queryset = Post.objects.all()
+
+    def get_queryset(self):
+        return Post.objects.all().order_by("-created_at")
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
